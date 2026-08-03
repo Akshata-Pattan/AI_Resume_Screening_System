4ec017e656ad1df1eb4ed80f3af9e1dc3453d4ca
Akshata <akshnp63@gmail.com>
2026-08-04 01:02:12 +0530

    Fix model loading

--- utils/semantic_match.py
+++ utils/semantic_match.py
@@ -1,14 +1,31 @@
 from sentence_transformers import SentenceTransformer, util
 
-# Load AI Model (loads only once)
-model = SentenceTransformer("all-MiniLM-L6-v2")
+# Model is NOT loaded during app startup
+model = None
+
+def get_model():
+    global model
+    if model is None:
+        model = SentenceTransformer("all-MiniLM-L6-v2")
+    return model
 
 
 def semantic_similarity(resume_text, job_description):
+    model = get_model()
 
-    embedding1 = model.encode(resume_text, convert_to_tensor=True)
-    embedding2 = model.encode(job_description, convert_to_tensor=True)
+    embedding1 = model.encode(
+        resume_text,
+        convert_to_tensor=True
+    )
 
-    similarity = util.cos_sim(embedding1, embedding2)
+    embedding2 = model.encode(
+        job_description,
+        convert_to_tensor=True
+    )
+
+    similarity = util.cos_sim(
+        embedding1,
+        embedding2
+    )
 
     return float(similarity[0][0] * 100)
